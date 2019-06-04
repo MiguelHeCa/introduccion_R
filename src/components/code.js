ddc08063c0b6c807ff411151caffa1bc0fe1b099
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2019-06-03 22:49:53 -0500

    Cambiando nombres de busqueda para codigos de ejercicios, soluciones y pruebas

--- src/components/code.js
+++ src/components/code.js
@@ -63,9 +63,9 @@ class CodeBlock extends React.Component {
     render() {
         const { Juniper, showSolution } = this.state
         const { id, source, solution, test, children } = this.props
-        const sourceId = source || `exc_${id}`
-        const solutionId = solution || `solution_${id}`
-        const testId = test || `test_${id}`
+        const sourceId = source || `ejr_${id}`
+        const solutionId = solution || `sln_${id}`
+        const testId = test || `pba_${id}`
         const juniperClassNames = {
             cell: classes.cell,
             input: classes.input,
