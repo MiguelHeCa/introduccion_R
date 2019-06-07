9f4fbe7405d12e235922b0c7cbaffc11f4c47159
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2019-06-06 22:40:01 -0500

    Agregando solucion y ejercicio de cadenas de caracteres

---
+++ exercises/ejr_01_06.R
@@ -0,0 +1,9 @@
+# Escribe el nombre de tu ciudad natal
+ciudad <- "__"
+___
+___
+
+# Convierte tres milésimas en notación científica a texto
+milesimas <- ___
+___
+___
---
+++ exercises/sln_01_06.R
@@ -0,0 +1,9 @@
+# Escribe el nombre de tu ciudad natal
+ciudad <- "Distrito Federal"
+class(ciudad)
+typeof(ciudad)
+
+# Convierte tres milésimas en notación científica a texto
+milesimas <- "3e-3"
+class(milesimas)
+typeof(milesimas)
